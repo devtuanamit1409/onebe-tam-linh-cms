d3f316e9fe13af8a37595fd8d2aa581a01054178
devtuanamit1409 <nguyentuanamit@gmail.com>
2024-11-06 11:58:23 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'strapi-app',
-      script: 'npm',
-      args: 'start',
+      name: "strapi-tam-linh",
+      script: "npm",
+      args: "start",
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
       },
     },
   ],
